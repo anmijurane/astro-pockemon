ccd24db82a79f3891f50bd49e4fee85ebcc2a47d
M. Andres Jurado <anmijurane@gmail.com>
2024-10-02 13:53:25 -0600

    INIT: astro proyect

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
+
+import solidJs from '@astrojs/solid-js';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [icon(), solidJs()]
+});
